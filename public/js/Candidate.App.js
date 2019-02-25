a37efa348753d7fd212e37fc8b28123f54765af2
buckhalton <isaiah.buckhalton@gmail.com>
2019-02-25 15:23:15 -0600

    adds basic styling.

--- public/js/Candidate.App.js
+++ public/js/Candidate.App.js
@@ -19,9 +19,13 @@ app.component("itmRoot", {
         calculatePercentage() {
             // For each candidate in the array, calculate the percentage
             this.candidates.forEach((candidate) => {
-                // Dividing each candidate's votes by the total amount of votes, and then multiplying by 100 for the percentage. 
-                candidate.percentage = (candidate.votes / totalVotes * 100).toFixed(2) + '%';
-            })
+                if (candidate.votes != 0) {
+                    // Dividing each candidate's votes by the total amount of votes, and then multiplying by 100 for the percentage. 
+                    candidate.percentage = (candidate.votes / totalVotes * 100).toFixed(2) + '%';
+                } else {
+                    candidate.percentage = "0%";
+                }
+            }) // end forEach
         } // end calculatePercentage
 
         onVote(candidate) {
@@ -40,11 +44,11 @@ app.component("itmRoot", {
         // I moved this outside of onAddCandidate, as it improves readability.
         candidateCheck(candidate) {
             //Looping through the candidates array
-            for(let i in this.candidates) {
-                 //if the candidate exists, return true
-                if(this.candidates[i].name.toLowerCase() === candidate.name.toLowerCase()) {
+            for (let i in this.candidates) {
+                //if the candidate exists, return true
+                if (this.candidates[i].name.toLowerCase() === candidate.name.toLowerCase()) {
                     return true;
-                } 
+                }
             } // end for in loop
             //Otherwise return false
             return false;
@@ -52,16 +56,16 @@ app.component("itmRoot", {
 
         onAddCandidate(candidate) {
             // If the field is empty, alert the user
-            if(candidate.name === undefined || candidate.name === "") {
+            if (candidate.name === undefined || candidate.name === "") {
                 alert("Please enter a name for the candidate!");
-            // If the candidate already exists, alert the user
-            } else if(this.candidateCheck(candidate)) {
+                // If the candidate already exists, alert the user
+            } else if (this.candidateCheck(candidate)) {
                 alert('That candidate already exists!');
-            // Else, if the field isn't empty, and the candidate doesn't exist, push the new candidate into the array.
+                // Else, if the field isn't empty, and the candidate doesn't exist, push the new candidate into the array.
             } else {
                 console.log(`Added candidate ${candidate.name}`);
                 // Using the spread syntax to add a new key-value pair for votes, and pushing the new candidate into the array.
-                this.candidates.push({...candidate});
+                this.candidates.push({ ...candidate });
                 console.log(this.candidates);
             }
         }// End onAddCandidate
@@ -69,9 +73,9 @@ app.component("itmRoot", {
         onRemoveCandidate(candidate) {
             console.log(`Removed candidate ${candidate.name}`);
             // Looping through the candidates array
-            for(let i in this.candidates) {
+            for (let i in this.candidates) {
                 // If the candidate that was clicked on matches a candidate in the array
-                if(candidate === this.candidates[i]) {
+                if (candidate === this.candidates[i]) {
                     // Remove that specific candidate from the array
                     this.candidates.splice(i, 1);
                     // Subtract that candidates votes from the total amount of votes
@@ -84,7 +88,7 @@ app.component("itmRoot", {
         } // end onRemoveCandidate
     },
     template: `
-        <h1>Which candidate brings the most joy?</h1>
+        <h1 class="jumbotron jumbotron-fluid">Which candidate brings the most joy?</h1>
              
         <itm-results 
             candidates="$ctrl.candidates">
@@ -132,24 +136,26 @@ app.component("itmManagement", {
         } // end removeCandidate
     },
     template: `
-        <h2>Manage Candidates</h2>
+        <div class="small-container">
+            <h2 class="header">Manage Candidates</h2>
 
-        <h3>Add New Candidate</h3>
-        <form ng-submit="$ctrl.submitCandidate($ctrl.newCandidate)" novalidate>
+            <h3>Add New Candidate</h3>
 
-            <label>Candidate Name</label>
-            <input type="text" ng-model="$ctrl.newCandidate.name" required>
+            <form class="form-group" ng-submit="$ctrl.submitCandidate($ctrl.newCandidate)" novalidate>
 
-            <button type="submit">Add</button>
-        </form>
+                <input placeholder="Candidate Name" type="text" ng-model="$ctrl.newCandidate.name" required>
 
-        <h3>Remove Candidate</h3>
-        <ul>
-            <li ng-repeat="candidate in $ctrl.candidates">
-                <span ng-bind="candidate.name"></span>
-                <button type="button" ng-click="$ctrl.removeCandidate(candidate)">X</button>
-            </li>
-        </ul>
+                <button type="submit" class="btn btn-outline-primary btn-sm">Add Candidate</button>
+            </form>
+
+            <h3>Remove Candidate</h3>
+            <ul class="list-group">
+                <li class="list-group-item candidate-remove" ng-repeat="candidate in $ctrl.candidates">
+                    <span ng-bind="candidate.name"></span>
+                    <button class="btn btn-outline-danger" type="button" ng-click="$ctrl.removeCandidate(candidate)">X</button>
+                </li>
+            </ul>
+        </div>
 
     `
 });
@@ -159,31 +165,37 @@ app.component("itmVote", {
         candidates: "<",
         onVote: "&"
     },
-    controller: class {},
+    controller: class { },
     template: `
-        <h2>Cast your vote!</h2>
+        <div class="small-container">
+            <h2>Cast your vote!</h2>
 
-        <button type="button"
-            ng-repeat="candidate in $ctrl.candidates"
-            ng-click="$ctrl.onVote({ $candidate: candidate })">
-            <span ng-bind="candidate.name"></span>
-        </button>
+            <button type="button"
+                class="btn btn-outline-dark" 
+                ng-repeat="candidate in $ctrl.candidates"
+                ng-click="$ctrl.onVote({ $candidate: candidate })">
+                <span ng-bind="candidate.name"></span>
+            </button>
+        <div>
     `
 });
 
 app.component("itmResults", {
     bindings: {
-        candidates: "<"
+        candidates: "<",
     },
-    controller: class {},
+
+    controller: class { },
     template: `
-        <h2>Live Results</h2>
-        <ul>
-            <li ng-repeat="candidate in $ctrl.candidates">
-                <span ng-bind="candidate.name"></span>
-                <strong ng-bind="candidate.votes"></strong>
-                <strong ng-bind="candidate.percentage"></strong>
-            </li>
-        </ul>
+        <div class="small-container">
+            <h2>Live Results</h2>
+            <ul class="list-group">
+                <li class="list-group-item vote-results" ng-repeat="candidate in $ctrl.candidates">
+                    <span ng-bind="candidate.name"></span>
+                    <strong ng-bind="candidate.votes"></strong>
+                    <strong ng-bind="candidate.percentage"></strong>
+                </li>
+            </ul>
+        <div>
     `
 });
